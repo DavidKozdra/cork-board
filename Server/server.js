752dd5fc9ade63b5a0421b9fd955932bfe2649e2
DavidKozdra <45106564+DavidKozdra@users.noreply.github.com>
2022-04-04 19:40:45 -0400

    Connection to Mongo
    Created and connected to MongoDB
    
    installed Mongodb
    installed dotenv

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
+require('dotenv').config();
+
+
+// Calling the required MongoDB module.
+const MongoClient = require("mongodb").MongoClient;
+
+// Server path
+const url = 'mongodb://' + process.env.CORK_MONGO_IP;
+
+// Name of the database
+const dbname = "corkboard";
+
+MongoClient.connect(url, (err,client)=>{
+  if(!err) {
+     console.log("successful connection with the server");
+  } else {
+     console.log("Error in the connectivity");
+     console.log(err);
+  }
+})
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', require('./routes'));
